e8cb5b4ca81b26f5572af90ed6b203f8021f14a4
Muhammad Hasif <mhasif175@gmail.com>
2022-04-28 18:21:36 +0800

    Done

--- Lab 2 StockTransaction.py
+++ Lab 2 StockTransaction.py
@@ -5,3 +5,24 @@
 
 #Profit = SellPrice(CurrentPrice*2000) - BuyPrice(0.40*2000) - TotalComm
 #Profit = -100 (made a loss of (-1*Profit)
+
+
+current_price = float(input("Enter current price for ABC Bank Corporation (S$): "))
+
+amount_paid = (2000 * 0.40)
+amount_sold = amount_paid * current_price
+paid_commission = amount_paid * 0.03
+selling_commission = (current_price * 2000) * 0.02
+total_commission = paid_commission + selling_commission
+profit = amount_sold - amount_paid - total_commission
+
+
+print("Your paid total commission of (S$): ", total_commission)
+print("You have made a profit of (S$): ", profit)
+
+
+
+
+
+
+
