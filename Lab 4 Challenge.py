e9ef40769a095b71b4a58a0cc6f4455686d3fd05
Muhammad Hasif <mhasif175@gmail.com>
2022-05-11 00:13:42 +0800

    hi

---
+++ Lab 4 Challenge.py
@@ -0,0 +1,7 @@
+s=int(input("enter a value: ")) #seconds
+h=s//3600 #hours
+m=(s-(h*3600))//60
+seconds=(s-(h*3600)-(m*60))
+print(s, "seconds is", h, "hours", m, "minutes and", seconds, "seconds")
+
+
