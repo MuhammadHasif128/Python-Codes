406d36603391f4c5534387115327260534b05b55
Muhammad Hasif <mhasif175@gmail.com>
2022-05-11 14:45:24 +0800

    hi

--- Lab 4 Challenge.py
+++ Lab 4 Challenge.py
@@ -4,4 +4,6 @@ m=(s-(h*3600))//60
 seconds=(s-(h*3600)-(m*60))
 print(s, "seconds is", h, "hours", m, "minutes and", seconds, "seconds")
 
+#a)runtime error, input validation text
+#b)while loop&for loop
 
---
+++ Practice.py
@@ -0,0 +1,22 @@
+#if_Q1
+#print("Welcome to the Club")
+#age=int(input("Please enter your age: "))
+
+#if age > 21:
+    #print("Enjoy your day")
+#elif age==21:
+    #print("Enjoy your day")
+#else:
+    #print("Adult Only. No Entry")
+
+
+
+
+
+
+
+
+
+
+
+
