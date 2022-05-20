43592eb0cf4859552973bedc4745fd7bcf429819
Muhammad Hasif <mhasif175@gmail.com>
2022-05-20 11:09:03 +0800

    hi

--- Lab 5 - TicketPrice.py
+++ Lab 5 - TicketPrice.py
@@ -1,32 +1,28 @@
-age = int(input("Please Enter Your Age:"))
+age = int(input("Please Enter Your Age: "))
+print("Monday is 1 to Sunday is 7")
 
-if age >=4 and age <=130:
-    day = int(input("Enter the day of the week:"))
-    if day >5 and day <=7:
-        print("Pay $10 sial")
-    elif day in range(1,6):
+while True:
+    if age > 130:
+        print("Too old. Invalid Age")
+        break
+    if age > 4 and age <= 130:
+        day = int(input("enter the day of the week: "))
+    if day >= 6 and day <= 7:
+        print("Pay $10")
+        break
+    elif day in range(1, 6):
         if age < 16:
             print("You have to pay $7.50")
-        elif age >=16 and age <65:
-            print("You have to pay $10.00")
+            break
+        elif age >= 16 and age < 65:
+            print("You have to pay $10")
+            break
         else:
-            print("You have to pay 5.50")
+            print("You have to pay $5.50")
+            break
+
 else:
-    print("Error, bye cb")
-
-
-
-
-
-
-
-
-if age < 16:
-    print("You have to pay $7.50")
-elif age >=16 and age <65:
-    print("You have to pay $10.00")
-else:
-    print("You have to pay 5.50")
+    print("Have a nice day")
 
 
 
--- Practice Workshop.py
+++ Practice Workshop.py
@@ -1,43 +1,8 @@
-"""age = int(input("Please Enter Your Age: "))
-print("Monday is 1 to Sunday is 7")
+age = int(input("Please Enter Your Age: "))
 
-while True:
-    if age > 130:
-        print("Too old. Invalid Age")
-        break
-    if age > 4 and age <= 130:
-        day = int(input("enter the day of the week: "))
-    if day >= 6 and day <= 7:
-        print("Pay $10")
-        break
-    elif day in range(1, 6):
-        if age < 16:
-            print("You have to pay $7.50")
-            break
-        elif age >= 16 and age < 65:
-            print("You have to pay $10")
-            break
-        else:
-            print("You have to pay $5.50")
-            break
-
+if age < 6:
+    print("Always follow your parents")
+elif age >= 6 and age < 21: #age <=20
+    print("Listen to your parents")
 else:
-    print("Have a nice day")"""
-
-"""""weight = float(input("Enter Your Weight: "))
-height = float(input("Enter your height: "))
-bmi = weight/height**2
-
-if bmi >= 27.5:
-    print("Obese")
-elif bmi >= 23 and bmi < 27.5:
-    print ("Overweight")
-elif bmi >= 18.5 and bmi < 23:
-    print("Normal")
-else:
-    print("Underweight")"""""
-
-height = float(input("Please input your height"))
-
-min_height =
-
+    print("You are an adult now. Behave responsibly.")
