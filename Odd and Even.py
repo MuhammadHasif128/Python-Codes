0a644b054ce22bc7c049b446f97f11acb39b4fe8
Muhammad Hasif <mhasif175@gmail.com>
2022-06-04 13:57:04 +0800

    hi

--- Odd and Even.py
+++ Odd and Even.py
@@ -10,3 +10,19 @@ for num in range(1, 6):
 
 print("Even #", even_count)
 print("Odd #", odd_count)
+
+"""even = 0
+odd = 0
+num = 1
+
+while num in range(1, 6):
+    x = int(input("Enter Number" + str(num) + ": "))
+    num += 1
+    if x % 2 == 0:
+        even = even + 1
+    else:
+        odd = odd + 1
+
+print("Even #", even)
+print("Odd #", odd)"""
+
