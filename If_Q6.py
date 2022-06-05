4c64db1af25b985e29ef993c0569742e1b7c9481
Muhammad Hasif <mhasif175@gmail.com>
2022-06-05 15:43:19 +0800

    hi

--- If_Q6.py
+++ If_Q6.py
@@ -1,6 +1,5 @@
 print("Welcome to python battle.")
 
-
 x = int(input("Please enter your queue number: "))
 
 if x % 2 == 1:
--- Practice Workshop.py
+++ Practice Workshop.py
@@ -1,9 +1,14 @@
-print("Welcome to python battle.")
+print("Welcome to python zoo.")
+name = input("Please Enter Your Name: ")
+age = int(input("Enter your age: "))
+day = int(input("Enter day (0-7): "))
 
-
-x = int(input("Please enter your queue number: "))
-
-if x % 2 == 1:
-    print("You belonged to Bravo.")
-else:
-    print("You belonged to Knight.")
+if 1 < day <= 5:
+    print("Hi", name, ",", "Please Pay $10")
+elif 6 <= day <=7:
+    if age < 21:
+        print("Hi", name, ",", "Please Pay $15")
+    elif 21 <= age <= 60:
+         print("Hi", name, ",", "Please Pay $60")
+    else:
+        print("Hi", name, ",", "Please Pay $20")
