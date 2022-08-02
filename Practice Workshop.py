246613c4937e31c55d5336739dc8b620810a968f
Muhammad Hasif <mhasif175@gmail.com>
2022-08-03 07:34:58 +0800

    hi

---
+++ Practice Workshop.py
@@ -0,0 +1,15 @@
+"""def add_two_num(num1, num2):
+     total = num1 + num2
+     return total
+num1 = int(input("Enter a number: "))
+num2 = int(input("Enter a number: "))
+total = add_two_num(num1, num2)
+print("total numbers is", total)"""
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964,
+  "yearr": 2020
+}
+print(len(thisdict))
