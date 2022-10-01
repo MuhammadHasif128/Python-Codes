cd23a0e34d1844e6de8b6e0d00ac99b5807133aa
Muhammad Hasif <mhasif175@gmail.com>
2022-10-01 16:19:49 +0800

    hi

--- Practice Workshop.py
+++ Practice Workshop.py
@@ -1,15 +1,54 @@
-"""def add_two_num(num1, num2):
-     total = num1 + num2
-     return total
-num1 = int(input("Enter a number: "))
-num2 = int(input("Enter a number: "))
-total = add_two_num(num1, num2)
-print("total numbers is", total)"""
+class Pokemon:
+  def __init__(self, name, type, skill):
+    self.name = name
+    self.type = type
+    self.skill = skill
 
-thisdict = {
-  "brand": "Ford",
-  "model": "Mustang",
-  "year": 1964,
-  "yearr": 2020
-}
-print(len(thisdict))
+  def Attack(self, damage):
+    return f"Pikachu attack using {self.skill} inflicting {damage} life points"
+
+  def __str__(self):
+    return f"Pokemon's name is {self.name} and pokemon type is {self.type}"
+
+pikachu = Pokemon("Pikcachu", "Electric", "Thunder Bolt")
+charmander = Pokemon("Charmander", "Fire", "Burn")
+
+
+print(pikachu.type)
+print(pikachu.skill)
+
+pikachu.skill = "Iron Fail"
+print(pikachu.skill)
+
+print(pikachu.Attack(10))
+
+
+print(pikachu)
+
+
+"""class Pokemon:
+  def __init__(self, name, type, skill):
+    self.name = name
+    self.type = type
+    self.skill = skill
+
+  def Attack(self, damage):
+    return f"Pikachu attack using {self.skill} inflicting {damage} life points"
+
+  def __str__(self):
+    return f"Pokemon's name is {self.name} and pokemon type is {self.type}"
+
+pikachu = Pokemon("Pikcachu", "Electric", "Thunder Bolt")
+charmander = Pokemon("Charmander", "Fire", "Burn")
+
+
+print(pikachu.type)
+print(pikachu.skill)
+
+pikachu.skill = "Iron Fail"
+print(pikachu.skill)
+
+print(pikachu.Attack(10))
+
+
+print(pikachu)"""
